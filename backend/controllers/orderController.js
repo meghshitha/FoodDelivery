3f2320211551a25098d09c1e5353c5e6367f5d63
Megha K <meghakemppanna@gmail.com>
2025-01-04 00:29:03 +0530

    UD

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,7 +5,7 @@ const Stripe= require('stripe')
 
 const stripe=new Stripe(process.env.SK_SECRET)
 const PlaceOrder = async (req,res)=>{
-    const frontend_url="http://localhost:5173"
+    const frontend_url="https://fooddelivery-user.onrender.com"
     try{
         const newOrder = await orderModel.create({
             userId:req.body.userId,
